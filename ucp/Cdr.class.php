ce421d2f88087933894f2a3457b07fce6dffac5c
kapil gupta <kgupta@sangoma.com>
2024-11-13 01:08:58 -0500

    Adding scribe license check

--- ucp/Cdr.class.php
+++ ucp/Cdr.class.php
@@ -87,7 +87,7 @@ class Cdr extends Modules{
 			"showPlayback" => $this->_checkPlayback() ? "1" : "0",
 			"showDownload" => $this->_checkDownload() ? "1" : "0",
 			"supportedHTML5" => implode(",",$this->UCP->FreePBX->Media->getSupportedHTML5Formats()),
-			"isScribeEnabled" =>$this->UCP->FreePBX->Modules->checkStatus("scribe")
+			"isScribeEnabled" => ($this->UCP->FreePBX->Modules->checkStatus("scribe") && $this->UCP->FreePBX->Scribe->isLicensed())
 		);
 	}
 
